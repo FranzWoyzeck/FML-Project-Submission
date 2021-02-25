63ca733615fb320e509a31132c5cafe7a4673847
Maximilian Richter <hy455@stud.uni-heidelberg.de>
2021-02-25 20:44:51 +0100

    Add DQN

--- agent_code/rule_based_agent/callbacks.py
+++ agent_code/rule_based_agent/callbacks.py
@@ -4,7 +4,7 @@ from random import shuffle
 import numpy as np
 
 
-def look_for_targets(free_space, start, targets, logger=None):
+def look_for_targets(free_space, start, targets):
     """Find direction of closest target that can be reached via free tiles.
 
     Performs a breadth-first search of the reachable free tiles until a target is encountered.
@@ -46,7 +46,7 @@ def look_for_targets(free_space, start, targets, logger=None):
                 frontier.append(neighbor)
                 parent_dict[neighbor] = current
                 dist_so_far[neighbor] = dist_so_far[current] + 1
-    if logger: logger.debug(f'Suitable target found at {best}')
+    #if logger: logger.debug(f'Suitable target found at {best}')
     # Determine the first step towards the best found target tile
     current = best
     while True:
@@ -63,7 +63,7 @@ def setup(self):
     You can also use the self.logger object at any time to write to the log
     file for debugging (see https://docs.python.org/3.7/library/logging.html).
     """
-    self.logger.debug('Successfully entered setup code')
+    #self.logger.debug('Successfully entered setup code')
     np.random.seed()
     # Fixed length FIFO queues to avoid repeating the same actions
     self.bomb_history = deque([], 5)
@@ -80,7 +80,7 @@ def act(self, game_state):
     which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
     what it contains.
     """
-    self.logger.info('Picking action according to rule set')
+    #self.logger.info('Picking action according to rule set')
 
     # Gather information about the game state
     arena = game_state['field']
@@ -119,7 +119,7 @@ def act(self, game_state):
     if (x, y) in valid_tiles: valid_actions.append('WAIT')
     # Disallow the BOMB action if agent dropped a bomb in the same spot recently
     if (bombs_left > 0) and (x, y) not in self.bomb_history: valid_actions.append('BOMB')
-    self.logger.debug(f'Valid actions: {valid_actions}')
+    #self.logger.debug(f'Valid actions: {valid_actions}')
 
     # Collect basic action proposals in a queue
     # Later on, the last added action that is also valid will be chosen
@@ -143,13 +143,13 @@ def act(self, game_state):
     if self.ignore_others_timer > 0:
         for o in others:
             free_space[o] = False
-    d = look_for_targets(free_space, (x, y), targets, self.logger)
+    d = look_for_targets(free_space, (x, y), targets)
     if d == (x, y - 1): action_ideas.append('UP')
     if d == (x, y + 1): action_ideas.append('DOWN')
     if d == (x - 1, y): action_ideas.append('LEFT')
     if d == (x + 1, y): action_ideas.append('RIGHT')
     if d is None:
-        self.logger.debug('All targets gone, nothing to do anymore')
+        #self.logger.debug('All targets gone, nothing to do anymore')
         action_ideas.append('WAIT')
 
     # Add proposal to drop a bomb if at dead end
