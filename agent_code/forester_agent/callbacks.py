935208c42f7c494a3019587db652a0ab09f87861
Johannes Pfeil <Johannes.Pfeil@gmx.de>
2021-03-09 19:14:04 +0100

    Add agent which trains via gradient boost
    After each round, all entries of the q-table are used to train new regressors (one for each action).

---
+++ agent_code/forester_agent/callbacks.py
@@ -0,0 +1,199 @@
+import os
+import pickle
+import random
+
+import numpy as np
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
+def setup_q_table(self):
+    # only create new q_table, if the is no according file
+    if not os.path.isfile("my-saved-q-table.pt"):
+        self.logger.info("Setting up q_table from scratch.")
+        self.q_table: dict = dict()
+    else:
+        self.logger.info("Loading q-table from saved state.")
+        with open("my-saved-q-table.pt", "rb") as file:
+            self.q_table:dict = dict(pickle.load(file))
+
+def setup_regressors(self):
+    # only create new regressors, if the is no according file
+    if not os.path.isfile("action_regressors.pt"):
+        self.logger.info("Setting up action_regressors from scratch.")
+        self.regressors: dict = dict()
+    else:
+        self.logger.info("Loading regressors from saved state.")
+        with open("action_regressors.pt", "rb") as file:
+            self.regressors:dict = dict(pickle.load(file))
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    setup_q_table(self)
+    setup_regressors(self)
+
+def get_action_via_regressors(self, game_state):
+    if not self.regressors:
+        return get_action_via_q_table(self, game_state)
+    self.logger.debug("choosing action via regressors.")
+    state = state_to_features(game_state)
+    Q = np.zeros(len(ACTIONS))
+    for i, a in enumerate(ACTIONS):
+        Q[i] = self.regressors[a].predict( [state] )
+    return ACTIONS[Q.argmax()]
+
+
+def get_action_via_q_table(self, game_state):
+    state = tuple(state_to_features(game_state).tolist())
+    if state in self.q_table:
+        self.logger.debug("State: "+ str(state)+ ", choosing move via q-table: " + ACTIONS[self.q_table[state].argmax()])
+        # self.logger.debug("q-values for cur state: "+str(self.q_table[S_string]))
+        return ACTIONS[self.q_table[state].argmax()]
+    self.logger.debug("No idea what to do. choosing randomly")
+    return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    random_prob = .1
+    if self.train and random.random() < random_prob:
+        self.logger.debug("Choosing action purely at random.")
+        # 80%: walk in any direction. 10% wait. 10% bomb.
+        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+    self.logger.debug("Querying model for action.")
+    # return get_action_via_q_table(self, game_state)
+    return get_action_via_regressors(self, game_state)
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+
+    n = 1   # number of fields the agent can see in each direction
+
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    # For example, you could construct several channels of equal shape, ...
+    channels = []
+
+    # Gather information about the game state
+    arena = game_state['field']
+    _, score, bombs_left, (x, y) = game_state['self']
+    # bombs = game_state['bombs']
+    # bomb_xys = [xy for (xy, t) in bombs]
+    # others = [xy for (n, s, b, xy) in game_state['others']]
+    coins = game_state['coins']
+
+    nearest_coin = get_nearest_coin(game_state)
+    radar = create_coin_radar(coins, (x,y), max_dist=5)
+
+    # Add selected features
+    
+    # channels.append(arena[x-n:x+n+1, y-n:y+n+1]) # field of view of short-sighted agent
+    # channels.append(radar)  # shows in wich direction are coins
+    
+    channels.append( get_position_case( (x,y) ) )   # add position case (4 possible cases)
+    channels.append( dist_to_proximity( get_direction_from_to( (x,y), nearest_coin, max_c=2 ), 3))
+    channels.append( (1, -1) )   # two constants that might help the linear value approximation
+    # channels.append( get_longest_direction(dir) )   # add direction in which agent has to go furthest
+
+
+                               
+
+    stacked_channels = np.stack(channels)
+    return stacked_channels.reshape(-1)
+
+def dist_to_proximity(pos:tuple, max_dist:int = 15)->tuple:
+    # converts relative position to proximity values (high values mean that the position is near)
+    a = max_dist - np.abs(pos[0])
+    b = max_dist - np.abs(pos[1])
+    if pos[0] < 0:
+        a *= -1
+    if pos[1] < 0:
+        b *= -1
+    return (a, b)
+
+def get_direction_from_to(p1:tuple, p2:tuple, max_c:int=2)->tuple:
+    # compute coordinates of vector from p1 to p2 and limit coordinates by max_c
+    a = max(min( p2[0]-p1[0], max_c), -max_c)
+    b = max(min( p2[1]-p1[1], max_c), -max_c)
+    return (a, b)
+
+def get_nearest_coin(state:dict):
+    # TODO catch case in which there are no more coins left
+    coins = state['coins'] 
+    _, _, _, (x,y) = state['self']
+    minDistToCoin = np.inf
+    nearestCoin = (0, 0)
+    for coin in coins:
+        dist = manhattenDist((x,y), coin)
+        if dist < minDistToCoin:
+            minDistToCoin = dist
+            nearestCoin = coin
+    return nearestCoin
+    
+def create_coin_radar(coins, pos, max_dist=np.inf):
+    rev_sorted_coins = coins.copy()
+    rev_sorted_coins.sort(reverse=True, key=lambda a: manhattenDist(a, pos))
+    radar = np.zeros((3,3))
+    for coin in rev_sorted_coins:
+        v = get_direction_from_to(pos, coin, 1)
+        d = min(manhattenDist(coin, pos), max_dist)
+        radar[v[0]+1,v[1]+1] = d
+    return radar
+
+def get_vector_from_to(p1, p2)->tuple:
+    try:
+        return (p2[0]-p1[0], p2[1]-p1[1])
+    except:
+        return (None,None)
+
+def get_longest_direction(v):
+    if (v[0] == None) or (v[1] == None):
+        return (None, None)
+    if np.abs(v[0]) > np.abs(v[1]):
+        return (1, 0)
+    else:
+        return (0, 1)
+
+def get_position_case(position):
+    a:bool = position[0] % 2
+    b:bool = position[1] % 2
+    return (a, b)
+
+def manhattenDist(p1, p2):
+    try:
+        return np.abs( p1[0]-p2[0] ) + np.abs( p1[1]-p2[1] )
+    except:
+        return None
---
+++ agent_code/forester_agent/train.py
@@ -0,0 +1,301 @@
+import numpy as np
+import pickle
+import random
+from collections import namedtuple, deque
+from typing import List
+from sklearn.ensemble import GradientBoostingRegressor
+
+import events as e
+from .callbacks import state_to_features, get_nearest_coin, manhattenDist, ACTIONS
+
+
+# import warnings filter from sklearn
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+ACTION_TO_INT = {'UP':0, 'RIGHT':1, 'DOWN':2, 'LEFT':3, 'WAIT':4, 'BOMB':5}
+
+# This is only an example!
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# Hyper parameters -- DO modify
+TRANSITION_HISTORY_SIZE: int = 3        # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS: float = 1.0   # record enemy transitions with probability ...
+ALPHA: float = 0.05                     # learning rate
+LIN_VAL_TRAINING_RATE = 0.05
+GAMMA:float = 0.9                       # Discount
+
+# Events
+DECREASE_COIN_DISTANCE = "DECREASE_COIN_DISTANCE"
+INCREASE_COIN_DISTANCE = "INCREASE_COIN_DISTANCE"
+DETECTED_LOOP = "DETECTED_LOOP"
+
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # Example: Setup an array that will note transition tuples
+    # (s, a, r, s')
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+
+    # setup regressor
+    
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    
+    if new_game_state and old_game_state:
+        # reward if distance to nearest coin decreased
+        old_nearest_coin = get_nearest_coin(old_game_state)
+        new_nearest_coin = get_nearest_coin(new_game_state)
+        _, _, _, old_pos = old_game_state['self']
+        _, _, _, new_pos = new_game_state['self']
+        
+        old_dist_to_coin = manhattenDist(old_pos, old_nearest_coin)
+        new_dist_to_coin = manhattenDist(new_pos, new_nearest_coin)
+        if old_dist_to_coin and new_dist_to_coin:   #makes sure the distance is not None, due to missing coins
+            if old_dist_to_coin > new_dist_to_coin:
+                events.append(DECREASE_COIN_DISTANCE)
+            else:
+                events.append(INCREASE_COIN_DISTANCE)
+
+        # check for loops in the agents behaviour, i.e. agent reaches a state twice
+        next_state_features = state_to_features(new_game_state)
+        if detect_loop(self, next_state_features):
+            events.append(DETECTED_LOOP)
+
+
+        ### append new transition. EVENTS HAVE TO BE ADDED BEFOREHAND ###
+        self.transitions.append(Transition(state_to_features(old_game_state), self_action, next_state_features, reward_from_events(self, events)))
+        q_val = q_learning(self)    # updates Q-value for correspronding game_state-action-pair
+        # q_val = SARSA(self)
+
+
+
+        # TODO: update Q_value for all analogue game_state - action-pairs
+        """
+        state_action_pairs =  get_analogue_game_states_action_pairs(old_game_state, self_action, self)
+        for s_a_pair in state_action_pairs:
+            state = s_a_pair[0]
+            action = s_a_pair[1]
+            #self.logger.debug("state: "+str(state))
+            #self.logger.debug("action: "+str(action))
+            s_string = str(state_to_features(s_a_pair[0]))
+            self.logger.debug("analogue state: " + s_string + str(ACTIONS[s_a_pair[1]]))
+            if s_string not in self.q_table:
+                self.q_table[s_string] = np.ones(6) +  np.random.rand(6)/10 # TODO: guess init values
+            self.q_table[s_string][action] = q_val
+        """
+
+def detect_loop(self, next_state_features):
+    for t in self.transitions:
+        if (next_state_features == t.next_state).all():
+            return True
+    return False
+
+def SARSA(self):
+    if len(self.transitions) < 2:
+        return
+    t = self.transitions[-2]    # contains('state', 'action', 'next_state', 'reward')
+    t_next = self.transitions[-1]
+    s_cur = str(t.state)
+    s_next = str(t_next.state)
+    a_cur = ACTION_TO_INT[t.action]
+    a_next = ACTION_TO_INT[t_next.action]
+    r = t.reward
+    # set initial (guessed) q-values for not yet visited states
+    if s_cur not in self.q_table:
+        self.q_table[s_cur] = np.ones(6) +  np.random.rand(6)/10
+    if s_next not in self.q_table:
+        self.q_table[s_next] = np.ones(6) + np.random.rand(6)/10
+    # calculate new q-value
+    q_s_cur_a_cur = self.q_table[s_cur][a_cur]
+    q_s_next_a_next = self.q_table[s_next][a_next]
+    q_val = q_s_cur_a_cur + ALPHA * ( r + GAMMA * q_s_next_a_next - q_s_cur_a_cur )
+    self.q_table[s_cur][a_cur] = q_val
+    return q_val
+
+# TODO implement n-step q-learning
+def q_learning(self):
+    if len(self.transitions) < 1:
+        return
+    t = self.transitions[-1]    # contains last ('state', 'action', 'next_state', 'reward')
+    s_cur = tuple(t.state.tolist())
+    s_next = tuple(t.next_state.tolist())
+    a = ACTION_TO_INT[t.action]
+    r = t.reward
+    # set initial q-values for not yet visited states
+    if s_cur not in self.q_table:
+        self.q_table[s_cur] = np.ones(6) +  np.random.rand(6)/10
+    if s_next not in self.q_table:
+        self.q_table[s_next] = np.ones(6) + np.random.rand(6)/10
+    # calculate new q-value
+    q_s_cur_a = self.q_table[s_cur][a]
+    q_s_next_a_best = self.q_table[s_next].argmax()
+    q_val = q_s_cur_a + ALPHA * ( r + GAMMA * q_s_next_a_best - q_s_cur_a )
+    self.q_table[s_cur][a] = q_val
+    return q_val
+    
+# TODO DEBUG
+def get_analogue_game_states_action_pairs(game_state, action:str, self, N=15):
+    state_action_pairs = []
+    new_state = game_state.copy()
+    new_action = action
+
+    for i in range(2): # mirrorings
+        state_action_pairs.append((new_state.copy(), ACTION_TO_INT[new_action]))
+
+        for j in range(3): # rotations
+            # rotate field
+            new_state['field'] = np.rot90(new_state['field'])
+            # rotate own position
+            name, score, bombs_left, (x, y) = new_state['self']
+            new_state['self'] = name, score, bombs_left, rot90_pos((x, y))
+            # TODO: rotate bomb positions
+            # TODO: rotate position of other players
+            # rotate coin positions
+            coins = new_state['coins']
+            new_state['coins'] = [rot90_pos(c) for c in coins]
+            # rotate action
+            new_action = rot90_action_string(new_action)
+            # add state and action to resp. arrays
+            state_action_pairs.append( (new_state.copy(), ACTION_TO_INT[new_action]) )
+
+        # mirror field
+        new_state['field'] = np.flipud(new_state['field'])
+        # mirror own position
+        name, score, bombs_left, (x, y) = new_state['self']
+        new_state['self'] = name, score, bombs_left, mirror_pos((x,y))
+        # TODO: mirror bomb positions
+        # TODO: mirror positions of other players
+        # mirror coin positions
+        coins = new_state['coins']
+        coins = [mirror_pos(c) for c in coins]
+        new_state['coins'] = coins
+        # mirror action
+        new_action = mirror_action_string(new_action)
+
+    return state_action_pairs    
+
+def update_training_batches(self, n_max_samples=np.inf):
+    # take randomly 10 samples from the q table and put the q-values in the according training_batches
+    # TODO idea: take only samples in accound with low standard deviation
+    n_samples = min(len(self.q_table), n_max_samples)
+    random_states_str = random.sample(list(self.q_table), n_samples)
+    temp = [s[1:-1] for s in random_states_str]  # remove brackets from the strings
+    features = [np.fromstring(s, dtype=float, sep=' ') for s in temp]   # convert strings back to numpy arrays
+
+    for i, key in enumerate(random_states_str):
+        for j, a in enumerate(ACTIONS):
+            self.training_batches[a][0].append( features[i] )
+            self.training_batches[a][1].append( self.q_table[key][j])
+
+def create_gradient_boost_regressor(self):
+    # TODO: not efficient. need to iterate through the q-table dictionary
+    key_list = list(self.q_table.keys())
+    X = np.array(list(map(list, key_list)))    # TODO conversion from keys to list to numpy array is inefficient.
+    self.logger.debug("X: " + str(X))
+    for a in ACTIONS:
+        y = [self.q_table[tuple(key.tolist())][ACTION_TO_INT[a]] for key in X]
+        regressor = GradientBoostingRegressor(random_state = 0)
+        regressor.fit(X, y)
+        self.regressors[a] = regressor
+
+
+# tested and works fine
+def mirror_pos(pos:tuple, N=15):
+    return (N-pos[0]+1, pos[1])
+
+# tested and works fine
+def rot90_pos(pos:tuple, N=15):
+    return (N-pos[1]+1 , pos[0])
+
+# Tested and works fine
+def mirror_action_string(action_string:str)->str:
+    action_int = ACTION_TO_INT[action_string]
+    if action_int < 4:
+        return ACTIONS[(action_int +2)%4]
+    else:
+        return ACTIONS[action_int]
+
+# tested and works fine
+def rot90_action_string(action_string:str)-> str:
+    action_int = ACTION_TO_INT[action_string]
+    if action_int < 4:
+        return ACTIONS[(action_int +1)%4]
+    else:
+        return ACTIONS[action_int]
+
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+
+    This is similar to reward_update. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+    self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
+
+    create_gradient_boost_regressor(self)
+
+    
+    # Store the q_table
+    with open("my-saved-q-table.pt", "wb") as file:
+        pickle.dump(self.q_table, file)
+    # store action vectors
+    with open("action_regressors.pt", "wb") as file:
+        pickle.dump(self.regressors , file)
+
+def reward_from_events(self, events: List[str]) -> int:
+    game_rewards = {
+        e.COIN_COLLECTED: 100,
+        e.DECREASE_COIN_DISTANCE: 1,
+        e.INCREASE_COIN_DISTANCE: -1,
+        e.KILLED_OPPONENT: 5,
+        e.INVALID_ACTION: -20,  #-20
+        e.MOVED_UP: -1,
+        e.MOVED_DOWN: -1,
+        e.MOVED_LEFT: -1,
+        e.MOVED_RIGHT: -1,
+        e.BOMB_DROPPED: -10,
+        e.WAITED: -20,
+        e.GOT_KILLED: -1000,   #-1000
+        e.DETECTED_LOOP: -1,
+        e.KILLED_SELF: -1000
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
